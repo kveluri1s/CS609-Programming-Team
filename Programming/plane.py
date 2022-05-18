2f6aee8942d86f290e07c740596adc6fa2cd66b6
dyenuganti1s <dyenuganti1s@semo.edu>
2022-05-18 15:27:16 -0500

    Create plane.py

---
+++ Programming/plane.py
@@ -0,0 +1,53 @@
+import pygame
+from game.constants import Constants
+
+class Plane(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
+  #  def __int__(self):
+        super().__init__()
+        self.frames = [
+            pygame.image.load('C:/Users/15732/PycharmProjects/flappybird/tappyplane/PNG/Planes/planeBlue1.png').convert_alpha(),
+            pygame.image.load('C:/Users/15732/PycharmProjects/flappybird/tappyplane/PNG/Planes/planeBlue2.png').convert_alpha(),
+            pygame.image.load('C:/Users/15732/PycharmProjects/flappybird/tappyplane/PNG/Planes/planeBlue3.png').convert_alpha()
+        ]
+        self.frame = 0
+        self.frame_count = 0
+        self.image = self.frames[self.frame]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.y_pos = Constants.screen_height/2
+        self.rect.center = (75, self.y_pos)
+        self.falling = True
+
+    def reset(self):
+        self.y_pos = Constants.screen_height / 2
+        self.rect.center = (75, self.y_pos)
+
+    def update(self):
+        self.frame_count += 1
+        if self.frame_count > Constants.plane_frame_rate:
+            self.frame += 1
+            self.frame_count = 0
+        if self.frame_count >= len(self.frames):
+            self.frame = 0
+        self.image = self.frames[self.frame]
+        self.mask = pygame.mask.from_surface(self.image)
+
+        if self.falling:
+            self.y_pos += Constants.plane_force2
+        else:
+            self.y_pos -= Constants.plane_force
+
+        self.rect.center = (75, self.y_pos)
+
+        self.falling = True
+
+    def apply_up(self):
+        self.falling = False
+
+
+
+
